3649592429f637258c725de7a2b2784ebdedb386
Andrei Amatuni <andrei.amatuni@gmail.com>
2016-06-23 12:51:57 -0400

    initial commit

---
+++ cha2aud.py
@@ -0,0 +1,185 @@
+import sys
+import re
+import datetime
+
+class Block:
+    def __init__(self, index):
+
+        self.index = index
+        self.num_clips = None
+        self.clips = []
+        self.sliced = False
+        #self.contains_fan_or_man = False
+        self.dont_share = False
+        self.id = ""
+        self.coder = None
+        self.lab_key = None
+        self.lab_name = None
+        self.old = False
+
+    def sort_clips(self):
+        self.clips.sort(key=lambda x: x.clip_index)
+
+class Clip:
+    def __init__(self, block_index, clip_index):
+        self.parent_audio_path = None
+        self.clan_file = None
+        self.block_index = block_index
+        self.clip_index = clip_index
+        self.clip_tier = None
+        self.multiline = False
+        self.multi_tier_parent = None
+        self.start_time = None
+        self.offset_time = None
+        self.end_time = None
+        self.timestamp = None
+        self.classification = None
+        self.gender_label = None
+        self.label_date = None
+        self.coder = None
+        self.lab_key = None
+
+
+
+
+the_block = None
+
+
+interval_regx = re.compile("\\x15\d+_\d+\\x15")
+
+
+def parse_clan(path):
+    global the_block
+    conversation = []
+
+    curr_conversation = []
+    with open(path, "rU") as file:
+        for line in file:
+            if line.startswith("@Bg:\tConversation {}".format(block_num)):
+                conversation.append(line)
+                continue
+            if conversation:
+                conversation.append(line)
+            if line.startswith("@Eg:\tConversation {}".format(block_num)):
+                break
+                # conversation.append(curr_conversation)
+                # curr_conversation = []
+
+    conversation_block = filter_conversation(conversation)
+
+    the_block = create_clips(conversation_block, block_num)
+
+    # for index, block in enumerate(conversation_block):
+    #     self.clip_blocks.append(self.create_clips(block, path, index + 1))
+
+    find_multitier_parents()
+
+def filter_conversation(conversation):
+
+    last_tier = ""
+
+    conv_block = []
+    for line in conversation:
+        if line.startswith("%"):
+            continue
+        elif line.startswith("@"):
+            continue
+        elif line.startswith("*"):
+            last_tier = line[0:4]
+            conv_block.append(line)
+        else:
+            conv_block.append(last_tier + line + "   MULTILINE")
+
+    return conv_block
+
+
+def find_multitier_parents():
+    for clip in the_block.clips:
+        if clip.multiline:
+            reverse_parent_lookup(the_block, clip)
+
+
+def reverse_parent_lookup(block, multi_clip):
+    for clip in reversed(block.clips[0:multi_clip.clip_index - 1]):
+        if clip.multiline:
+            continue
+        else:
+            multi_clip.multi_tier_parent = clip.timestamp
+            return
+
+
+def create_clips(clips, block_index):
+
+    block = Block(block_index)
+
+    for index, clip in enumerate(clips):
+
+        curr_clip = Clip(block_index, index + 1)
+        curr_clip.clip_tier = clip[1:4]
+        if "MULTILINE" in clip:
+            curr_clip.multiline = True
+
+        interval_reg_result = interval_regx.search(clip)
+        if interval_reg_result:
+            interval_str = interval_reg_result.group().replace("\x15", "")
+            curr_clip.timestamp = interval_str
+
+        time = interval_str.split("_")
+        time = [int(time[0]), int(time[1])]
+
+        final_time = ms_to_hhmmss(time)
+
+        curr_clip.start_time = str(final_time[0])
+        curr_clip.end_time = str(final_time[1])
+        curr_clip.offset_time = str(final_time[2])
+
+        block.clips.append(curr_clip)
+
+    block.num_clips = len(block.clips)
+
+    for clip in block.clips:
+        if clip.clip_tier == "FAN":
+            block.contains_fan_or_man = True
+        if clip.clip_tier == "MAN":
+            block.contains_fan_or_man = True
+
+    return block
+
+
+def ms_to_hhmmss(interval):
+    x_start = datetime.timedelta(milliseconds=interval[0])
+    x_end = datetime.timedelta(milliseconds=interval[1])
+
+    x_diff = datetime.timedelta(milliseconds=interval[1] - interval[0])
+
+    start = ""
+    if interval[0] == 0:
+        start = "0" + x_start.__str__()[:11] + ".000"
+    else:
+
+        start = "0" + x_start.__str__()[:11]
+        if start[3] == ":":
+            start = start[1:]
+    end = "0" + x_end.__str__()[:11]
+    if end[3] == ":":
+        end = end[1:]
+
+    return [start, end, x_diff]
+
+
+def to_audacity_labels(block):
+
+
+    with open(output, "wb") as out:
+        for clip in block.clips:
+            out.write("{}\t{}\t{}".format(clip.start_time, clip.offset_time ))
+
+if __name__ == "__main__":
+
+    block_num = sys.argv[1]
+    cha_file = sys.argv[2]
+    output = sys.argv[3]
+
+    parse_clan(cha_file)
+
+    print
